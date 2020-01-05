3609bcb93a0d90d9b9429caf0786c55d20ce4aab
Sabda PS <sabdaps@gmail.com>
2020-01-06 01:05:52 +0700

    Update core.clj

--- clojure/app/src/app/core.clj
+++ clojure/app/src/app/core.clj
@@ -1,6 +1,16 @@
 (ns app.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn p001
+  []
+  (- (+ (reduce + (range 3 1000 3))
+        (reduce + (range 5 1000 5)))
+     (reduce + (range 15 1000 15))))
+
+(defn p002
+  "return the sum of fibo numbers that satisfy f below lim"
+  [lim f]
+  (->> (iterate #(let [[a b] %] [b (+ a b)]) [0 1])
+       (map first)
+       (filter f)
+       (take-while #(< % lim))
+       (reduce +)))
